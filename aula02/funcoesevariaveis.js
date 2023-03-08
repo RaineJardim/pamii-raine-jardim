0bfcb14755e02b5629b26e2366d0c4b59cc511f8
RaineJardim <rainejardim@hotmail.com>
2023-03-08 08:38:34 -0300

    Aula sobre JavaScript com variaveis e funções

---
+++ aula02/funcoesevariaveis.js
@@ -0,0 +1,18 @@
+var variavel1 = "Olá"
+let variavel2 = 3
+const variavel3 = 8
+
+const obj = {
+    nome: "Raíne",
+    signo: "Virgem"
+}
+
+console.log(variavel1 + " turminha do " + variavel2+ "° ano")
+console.log(variavel2 + variavel3)
+console.log(typeof variavel1)
+console.log(typeof variavel2)
+console.log(typeof variavel3)
+
+// Formato JSON - JavaScript Notation Object
+console.log(obj.nome, obj.signo)
+console.log(typeof obj)
