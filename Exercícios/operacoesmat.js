d17bfc15de2d85a7f4e705ff9074e44afe54cbc5
RaineJardim <rainejardim@hotmail.com>
2023-03-11 17:29:34 -0300

    Exercício de Javascript com as funções matemáticas: Soma, Subtração, Multiplicação, Divisão, Potencia e Raiz Quadrada

---
+++ Exercícios/operacoesmat.js
@@ -0,0 +1,64 @@
+// Soma a + b --------------------------------------------------------
+
+const funcaoSoma = (a, b) => {
+
+    return a + b
+
+}
+
+console.log ("Soma:", funcaoSoma(2, 10))
+
+
+// Subtração a - b ---------------------------------------------------
+
+const funcaoSubtracao = (a, b) => {
+
+    return a - b
+
+}
+
+console.log ("Subtração:", funcaoSubtracao(18,7))
+
+
+// Multiplicação a * b -----------------------------------------------
+
+const funcaoMultiplicacao = (a, b) => {
+
+    return a * b
+
+}
+
+console.log ("Multiplicação:", funcaoMultiplicacao(5,5))
+
+
+// Divisão a / b -----------------------------------------------------
+
+const funcaoDivisao = (a, b) => {
+
+    return a/b
+
+}
+
+console.log ("Divisão:", funcaoDivisao(10,2))
+
+
+// Potenciação a ^ b -------------------------------------------------
+
+const funcaoPotencia = (a, b) => {
+
+    return Math.pow(a, b)
+
+}
+
+console.log ("Potenciação:", funcaoPotencia(4,2))
+
+
+// Raiz Quadrada √a --------------------------------------------------
+
+const funcaoRaizQuadrada = (a) => {
+
+    return Math.sqrt(a)
+
+}
+
+console.log ("Raiz Quadrada:", funcaoRaizQuadrada(25))
